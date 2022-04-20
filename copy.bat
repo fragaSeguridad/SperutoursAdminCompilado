9243fe832be7e51639b739bb635116368fedccd9
fragaSeguridad <102570363+fragaSeguridad@users.noreply.github.com>
2022-04-20 09:52:19 -0500

    asd

--- copy.bat
+++ copy.bat
@@ -1,5 +1,19 @@
+del *.eot
+del *.svg
+del *.woff2
+del *.woff
+del *.ico
+del *.txt
+del *.html
+del *.js
+del *.css
+del *.ttf
+ 
+RMDIR /Q/S "assets"
+ 
+xcopy  /E /I /C   D:\_Proyectos\_Proyectos2022\_Sperutours\SperutoursAdmin\dist\sperutours-admin\assets D:\_Proyectos\_Proyectos2022\_Sperutours\SperutoursAdminCompilado\assets
 
-copy  C:\xampp\htdocs\sperutours_maquetado.github.io\*.*  *.* /y
+copy  D:\_Proyectos\_Proyectos2022\_Sperutours\SperutoursAdmin\dist\sperutours-admin\*.*  *.* /y
 (pause)
 
  
